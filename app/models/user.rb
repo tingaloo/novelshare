20bcfb51d85afbba5c9a0e765409bd5bc2fa9920
Ting Lew <ting.lew@gmail.com>
2015-06-11 12:24:30 -0400

    sign_up_page drafted

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
+        # removed :validatable
 
   has_many :books
   has_many :book_checkouts
