88818c4489f3566e9d583c315f24fdcd8b5964a3
Ting Lew <ting.lew@gmail.com>
2015-06-17 11:31:45 -0400

    proper migration added

--- db/migrate/20150617152643_fix_update_index_on_users.rb
+++ db/migrate/20150617152643_fix_update_index_on_users.rb
@@ -1,4 +1,7 @@
 class FixUpdateIndexOnUsers < ActiveRecord::Migration
   def change
+    sql = 'CREATE INDEX index_users_on_email'
+    sql << ' ON users' if Rails.env == 'production' # Heroku pg
+    ActiveRecord::Base.connection.execute(sql)
   end
 end
