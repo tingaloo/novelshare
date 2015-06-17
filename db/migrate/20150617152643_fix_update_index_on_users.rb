388e32938c3e6588e8e2a75f28775314d260d619
Ting Lew <ting.lew@gmail.com>
2015-06-17 13:29:50 -0400

    literally removed def change in corrupt files

--- db/migrate/20150617152643_fix_update_index_on_users.rb
+++ db/migrate/20150617152643_fix_update_index_on_users.rb
@@ -1,7 +1,3 @@
 class FixUpdateIndexOnUsers < ActiveRecord::Migration
-  def change
-    sql = 'CREATE INDEX index_users_on_email'
-    sql << ' ON users' if Rails.env == 'production' # Heroku pg
-    ActiveRecord::Base.connection.execute(sql)
-  end
+
 end
